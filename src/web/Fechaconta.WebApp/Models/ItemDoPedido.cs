92188a01b672cacce373d90d6ab250bbe20b01c9
Higor Nucci <higornucci@gmail.com>
2015-08-21 11:16:46 -0400

    envia itens do pedido para comanda

---
+++ src/web/Fechaconta.WebApp/Models/ItemDoPedido.cs
@@ -0,0 +1,13 @@
+﻿namespace Fechaconta.WebApp.Models
+{
+    public class ItemDoPedido
+    {
+        public Item Item { get; set; }
+        public int Quantidade { get; set; }
+
+        public double Valor 
+        {
+            get { return Item.Valor * Quantidade; }
+        }
+    }
+}
