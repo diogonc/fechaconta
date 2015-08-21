cd8f456cbc5a9a82d5452c01d7b7c523d886b3e6
Robson Castilho <robsoncastilho@yahoo.com.br>
2015-08-21 11:30:47 -0400

    Movidos de pasta

---
+++ src/web/Fechaconta.WebApp/Models/ComandaRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fechaconta.WebApp.Models
+{
+    public static class ComandaRepositorio
+    {
+        private static readonly List<Comanda> Comandas = new List<Comanda>();
+
+        public static Comanda BuscarPor(string numeroDaComanda)
+        {
+            return Comandas.FirstOrDefault(c => c.Numero == numeroDaComanda);
+        }
+
+        public static void Adicionar(Comanda comanda)
+        {
+            Comandas.Add(comanda);
+        }
+    }
+}
