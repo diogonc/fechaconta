c6a5474b633fba2291f24c70a0c6fa4d97801590
Robson Castilho <robsoncastilho@yahoo.com.br>
2015-08-21 11:27:55 -0400

    Refactorinng

--- src/web/Fechaconta.WebApp/Controllers/CardapioController.cs
+++ src/web/Fechaconta.WebApp/Controllers/CardapioController.cs
@@ -30,7 +30,6 @@ namespace Fechaconta.WebApp.Controllers
             cardapio.Adicionar("Sobremesas", "Surpresa de Limão", "Sufle de Limão com raspas de chocolate.", 5.10);
             cardapio.Adicionar("Sobremesas", "Petit gâteau", "Doce de Leite, Chocollate em Barras com Sorvete.", 5.10);
 
-
             cardapio.Adicionar("Bebidas", "Skol", "Bem geladinha.", 2.5);
             cardapio.Adicionar("Bebidas", "Antartica", "Mais geladinha ainda.", 3);
 
