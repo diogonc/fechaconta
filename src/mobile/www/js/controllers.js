3f4d63aa8b52bcdedcac585c4788c9773f6b4b97
Diogo <diogonc@gmail.com>
2015-08-21 10:12:01 -0400

    permitindo acesso de fora

--- src/mobile/www/js/controllers.js
+++ src/mobile/www/js/controllers.js
@@ -2,19 +2,12 @@ angular.module('starter.controllers', [])
 
 .controller('ComandaCtrl', function($scope) {})
 
-.controller('MenuCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('MenuCtrl', function($scope, Cardapio) {
+  $scope.$on('$ionicView.enter', function(e) {
+  });
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+  $scope.itens = Cardapio.all();
+
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
