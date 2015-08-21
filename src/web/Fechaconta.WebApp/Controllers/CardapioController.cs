67dd17631b726fecf9fbdaefbe6c0e9a3d260415
AnselmoPfeifer <contato@anselmopfeifer.com>
2015-08-21 10:21:59 -0300

    Add novos itens ao Cardapio

--- src/web/Fechaconta.WebApp/Controllers/CardapioController.cs
+++ src/web/Fechaconta.WebApp/Controllers/CardapioController.cs
@@ -22,6 +22,14 @@ namespace Fechaconta.WebApp.Controllers
             cardapio.Adicionar("Bolinhos", "Bolinho de Feijoada", "Recheado com couve e bacon.", 7.56);
             cardapio.Adicionar("Bolinhos", "Bolinho de Comitiva", "Bolinho de arroz com carne de sol e queijo coalho.", 11);
 
+            cardapio.Adicionar("Saladas", "Salada do Cezinha", "Folhas verdes, molho de mostarda, tomate, Torradinha e Queijo de Minas.", 11.87);
+            cardapio.Adicionar("Saladas", "Salada de Carrapicho", "Filé cortado finim acompanhado de Minirrúculas, Parmessão e Alho.", 10.69);
+            cardapio.Adicionar("Saladas", "Doces Pantaneiro", "Queijo caipira fresco com Goiabada.", 9.66);
+
+            cardapio.Adicionar("Sobremesas", "Surpresa de Limão", "Sufle de Limão com raspas de chocolate.", 5.10);
+            cardapio.Adicionar("Sobremesas", "Petit gâteau", "Doce de Leite, Chocollate em Barras com Sorvete.", 5.10);
+
+
             cardapio.Adicionar("Bebidas", "Skol", "Bem geladinha.", 2.5);
             cardapio.Adicionar("Bebidas", "Antartica", "Mais geladinha ainda.", 3);
 
