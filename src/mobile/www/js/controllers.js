775ea61c6eefc1698d57d7c7b58d5a90656e4f78
Diogo <diogonc@gmail.com>
2015-08-21 10:01:32 -0400

    organizando estruturas dos arquivos

--- src/mobile/www/js/controllers.js
+++ src/mobile/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('ComandaCtrl', function($scope) {})
 
 .controller('MenuCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
