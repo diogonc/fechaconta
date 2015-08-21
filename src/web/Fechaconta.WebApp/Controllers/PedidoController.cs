6c33fac56997f1c1b205389788cf7853e60b2aff
Higor Nucci <higornucci@gmail.com>
2015-08-21 11:27:22 -0400

    envia itens do pedido para comanda

--- src/web/Fechaconta.WebApp/Controllers/PedidoController.cs
+++ src/web/Fechaconta.WebApp/Controllers/PedidoController.cs
@@ -11,9 +11,14 @@ namespace Fechaconta.WebApp.Controllers
 {
     public class PedidoController : ApiController
     {
-        public HttpResponseMessage Post([FromBody] Pedido pedido)
+        public HttpResponseMessage Post(Pedido pedido)
         {
             var comanda = ComandaRepositorio.BuscarPor(pedido.NumeroDaComanda);
+            if(comanda == null)
+            {
+                comanda = new Comanda { Numero = pedido.NumeroDaComanda };
+                ComandaRepositorio.Adicionar(comanda);
+            }
             comanda.Adicionar(pedido);
 
             return new HttpResponseMessage(HttpStatusCode.OK);
--- src/web/Fechaconta.WebApp/Models/Pedido.cs
+++ src/web/Fechaconta.WebApp/Models/Pedido.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 
 namespace Fechaconta.WebApp.Models
 {
-    [Serializable]
     public class Pedido
     {
         public string NumeroDaComanda { get; set; }
