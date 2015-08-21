26024f61c620387620aa183a47ff5aeaab252a1b
Higor Nucci <higornucci@gmail.com>
2015-08-21 09:29:35 -0400

    Cardapio criado

---
+++ src/web/Fechaconta.WebApp/Controllers/CardapioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Fechaconta.WebApp.Models;
+
+namespace Fechaconta.WebApp.Controllers
+{
+    public class CardapioController : ApiController
+    {
+        // GET api/values
+        public Cardapio Get()
+        {
+
+            return CriadorDeCardapio.Criar();
+        }
+    }
+
+    public class CriadorDeCardapio
+    {
+        public static Cardapio Criar()
+        {
+            var cardapio = new Cardapio();
+
+            //var bolinhoDeFeijoada = new Item() { Nome}
+
+            return cardapio;
+        }
+    }
+}
