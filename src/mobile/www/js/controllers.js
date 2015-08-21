bda60a6ae85b5b62f7b212e65df5bca3d2b08332
Higor Nucci <higornucci@gmail.com>
2015-08-21 16:40:46 -0400

    Adicionando a quantidade

--- src/mobile/www/js/controllers.js
+++ src/mobile/www/js/controllers.js
@@ -20,13 +20,13 @@ angular.module('starter.controllers', [])
 
   $scope.$on('$ionicView.enter', function(e) {
       $http.get(homeUrl+'api/cardapio').success(function(data){
-        $scope.itens = data;
+        $scope.itens = data;      
+        inicializarQuantidade();
       });
-
       comanda = PedidoRepository.getAll()[0];
       $scope.mesa = comanda.mesa;
-
       pedido = new Pedido(comanda.numeroDaComanda, comanda.mesa);
+      
   });
 
   $scope.categoriaSelecionada = 0;
@@ -37,10 +37,25 @@ angular.module('starter.controllers', [])
 
   $scope.adicionarItem = function(item){    
     pedido.adicionar(item);
+    item.quantidade += 1;
   };
 
   $scope.removerItem = function(item){
     pedido.remover(item);
-  }
+    if(item.quantidade > 0) {
+        item.quantidade -= 1;
+    }
+  };
 
+  function inicializarQuantidade() {
+      console.log('foi', $scope.itens);
+      
+     for (var i = 0; i < $scope.itens.Categorias.length; i++) {
+         var categoria= $scope.itens.Categorias[i];
+         for (var j = 0; j < categoria.Itens.length; j++) {
+            categoria.Itens[j].quantidade = 0;
+         }
+     }
+ };
+      
 });
