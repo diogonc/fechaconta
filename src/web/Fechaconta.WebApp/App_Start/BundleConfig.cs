8b077cca46e4dedf79e6bd54065d2f12d7f74290
Robson Castilho <robsoncastilho@yahoo.com.br>
2015-08-22 06:01:41 -0400

    Correcao referencias css

--- src/web/Fechaconta.WebApp/App_Start/BundleConfig.cs
+++ src/web/Fechaconta.WebApp/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace Fechaconta.WebApp
 {
@@ -20,9 +19,7 @@ namespace Fechaconta.WebApp
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
         }
     }
 }
